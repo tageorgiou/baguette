e9f57c08d1fb3aebc78325beb176807f0a1cec4c
Joshua Ma <me@joshma.com>
2012-02-03 03:10:18 -0500

    trying mongo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import json
 import base64
+import datetime
 
 from flask import Flask, request, redirect
 from mongokit import Connection, Document
@@ -12,6 +13,7 @@ FB_DOMAIN = 'https://baguette.herokuapp.com/'
 app = Flask(__name__)
 app.debug = True
 
+con = Connection()
 
 # Page unauthenticated users land at.
 @app.route('/start', methods=['GET', 'POST'])
@@ -24,8 +26,24 @@ def main():
     if not signed_req_raw:
         return '', 400
     signed_req = json.parse(signed_req_raw)
+    fb_id = signed_req['user_id']
+    oauth_token = signed_req['auth_token']
+    user = User()
+    user['fb_id'] = fb_id
+    user['oauth_token'] = oauth_token
+    user.save()
     raise
     return 'welcome'
+
+@con.register
+class User(Document):
+    structure = {
+        'fb_id': unicode,
+        'oauth_token': unicode,
+        'date_creation': datetime.datetime,
+    }
+    required_fields = ['name', 'fb_id', 'oauth_token', 'date_creation']
+    default_valeus = {'date_creation': datetime.datetime.utcnow}
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
