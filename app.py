f7654de8c1fc5c23842272598b61d8b92271484d
Joshua Ma <me@joshma.com>
2012-02-03 05:24:03 -0500

    testing

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ def main():
     access_token, expires = [x[0] for x in param.values()]
 
     resp, content = h.request(ME_URL % access_token)
-    return "URL: " + (ME_URL % access_token)
     fb_id = json.loads(content)['id']
+    return "URL: " + (ME_URL % access_token + "<br />ID: " + fb_id)
 
     user = db.users.find_one({'fb_id': fb_id})
     created = 'Updated existing'
